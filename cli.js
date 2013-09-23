d3a770c2e821cc47c4328f01114d8aa73d775801
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-09-23 11:15:13 +0200

    usage

--- cli.js
+++ cli.js
@@ -14,18 +14,19 @@ function green(txt){
     return "\x1b[32m" + txt + "\x1b[0m";
 }
 
-var usage = "$ rename [--find <pattern>] [--replace <string>] [--dry-run] [--regex] <files>\n\
+var usage = "Usage: \n\
+$ rename [--find <pattern>] [--replace <string>] [--dry-run] [--regex] <files>\n\
 \n\
 -f, --find      The find string, or regular expression when --regex is set.\n\
                 If not set, the whole filename will be replaced.\n\
--r, --replace   The replace string. With --regex, --replace can reference\n\
+-r, --replace   The replace string. With --regex set, --replace can reference\n\
                 parenthesised substrings from --find with $1, $2, $3 etc.\n\
                 If omitted, defaults to a blank string. The special token\n\
                 '{{index}}' will insert an incrementing number per file\n\
                 processed.\n\
 -e, --regex     When set, --find is intepreted as a regular expression.\n\
--d, --dry-run   Used for test runs. When set, rename does everything but\n\
-                rename the file.\n";
+-d, --dry-run   Used for test runs. Set to do everything but rename the file.\n\
+-h, --help      Print usage instructions.\n";
 
 /*
 TODO: Tidy error handling
