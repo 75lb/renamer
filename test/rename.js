a562487fbb4329a6c6129e74d3329c4dc563ad51
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-03-26 18:03:40 +0100

    refactor, new test suite WIP

---
+++ test/rename.js
@@ -0,0 +1,64 @@
+var test = require("tape"),
+    renamer = require("../lib/renamer"),
+    Options = require("../lib/RenamerOptions");
+
+var preset = {
+    one: [ "file1.txt", "file2.txt", "folder/file3.txt"]
+};
+
+test("find string not found, nothing replaced", function(t){
+    var options = new Options({
+        files: preset.one,
+        find: "blah",
+        replace: "clive"
+    });
+    
+    var results = renamer.rename(options);
+    t.deepEqual(results, [
+        { before: "file1.txt" },
+        { before: "file2.txt" },
+        { before: "folder/file3.txt" }
+    ]);
+
+    t.end();
+});
+
+test("simple find and replace", function(t){
+    var options = new Options({
+        files: preset.one,
+        find: "file",
+        replace: "clive"
+    });
+    
+    var results = renamer.rename(options);
+    t.deepEqual(results, [
+        { before: "file1.txt", after: "clive1.txt" },
+        { before: "file2.txt", after: "clive2.txt" },
+        { before: "folder/file3.txt", after: "folder/clive3.txt" }
+    ]);
+    t.end();
+});
+
+test("case insensitive", function(t){
+    var options = new Options({
+        files: preset.one,
+        find: "FILE",
+        replace: "clive"
+    });
+    
+    var results = renamer.rename(options);
+    t.deepEqual(results, [
+        { before: "file1.txt" },
+        { before: "file2.txt" },
+        { before: "folder/file3.txt" }
+    ]);
+
+    options.insensitive = true;
+    results = renamer.rename(options);
+    t.deepEqual(results, [
+        { before: "file1.txt", after: "clive1.txt" },
+        { before: "file2.txt", after: "clive2.txt" },
+        { before: "folder/file3.txt", after: "folder/clive3.txt" }
+    ]);
+    t.end();
+});
